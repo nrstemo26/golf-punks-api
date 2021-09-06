bfe4f67de65a32615a66642957a9ae43b7ceff73
BNB Punks <bnbpunksdev@gmail.com>
2021-09-06 10:16:46 -0700

    updated image url

--- src/database.js
+++ src/database.js
@@ -1,26 +1,42 @@
 module.exports = {
-  "0": {
-    "name": "Chunky Monkey",
-    "birthday": "10-18"
-  },
-  "1": {
-    "name": "Chunky Monkey",
-    "birthday": "10-18"
-  },
-  "2": {
-    "name": "Flerp Derp",
-    "birthday": "01-08"
-  },
-  "3": {
-    "name": "Buttle McFuddle",
-    "birthday": "01-22"
-  },
-  "4": {
-    "name": "Quiero Pancakes",
-    "birthday": "03-04"
-  },
-  "5": {
-    "name": "Guino the Penguin",
-    "birthday": "03-09"
-  }
+		"0": {
+			"name": "Loot #0",
+			"birthday": "01-08"
+		},
+		"1": {
+			"name": "Loot #1",
+			"birthday": "01-08"
+		},
+		"2": {
+			"name": "Loot #2",
+			"birthday": "01-08"
+		},
+		"3": {
+			"name": "Loot #3",
+			"birthday": "01-08"
+		},
+		"4": {
+			"name": "Loot #4",
+			"birthday": "01-08"
+		},
+		"5": {
+			"name": "Loot #5",
+			"birthday": "01-08"
+		},
+		"6": {
+			"name": "Loot #6",
+			"birthday": "01-08"
+		},
+		"7": {
+			"name": "Loot #7",
+			"birthday": "01-08"
+		},
+		"8": {
+			"name": "Loot #8",
+			"birthday": "01-08"
+		},
+		"9": {
+			"name": "Loot #9",
+			"birthday": "01-08"
+		}
 }
