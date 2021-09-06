010e9f307e3f9e851bab6129853b55d348668843
BNB Punks <bnbpunksdev@gmail.com>
2021-09-06 10:28:30 -0700

    updated image url

--- src/database.js
+++ src/database.js
@@ -1,42 +1,402 @@
 module.exports = {
 		"0": {
 			"name": "Loot #0",
-			"birthday": "01-08"
+			"birthday": "10-24"
 		},
 		"1": {
 			"name": "Loot #1",
-			"birthday": "01-08"
+			"birthday": "10-24"
 		},
 		"2": {
 			"name": "Loot #2",
-			"birthday": "01-08"
+			"birthday": "10-24"
 		},
 		"3": {
 			"name": "Loot #3",
-			"birthday": "01-08"
+			"birthday": "10-24"
 		},
 		"4": {
 			"name": "Loot #4",
-			"birthday": "01-08"
+			"birthday": "10-24"
 		},
 		"5": {
 			"name": "Loot #5",
-			"birthday": "01-08"
+			"birthday": "10-24"
 		},
 		"6": {
 			"name": "Loot #6",
-			"birthday": "01-08"
+			"birthday": "10-24"
 		},
 		"7": {
 			"name": "Loot #7",
-			"birthday": "01-08"
+			"birthday": "10-24"
 		},
 		"8": {
 			"name": "Loot #8",
-			"birthday": "01-08"
+			"birthday": "10-24"
 		},
 		"9": {
 			"name": "Loot #9",
-			"birthday": "01-08"
+			"birthday": "10-24"
+		},
+		"10": {
+			"name": "Loot #10",
+			"birthday": "10-24"
+		},
+		"11": {
+			"name": "Loot #11",
+			"birthday": "10-24"
+		},
+		"12": {
+			"name": "Loot #12",
+			"birthday": "10-24"
+		},
+		"13": {
+			"name": "Loot #13",
+			"birthday": "10-24"
+		},
+		"14": {
+			"name": "Loot #14",
+			"birthday": "10-24"
+		},
+		"15": {
+			"name": "Loot #15",
+			"birthday": "10-24"
+		},
+		"16": {
+			"name": "Loot #16",
+			"birthday": "10-24"
+		},
+		"17": {
+			"name": "Loot #17",
+			"birthday": "10-24"
+		},
+		"18": {
+			"name": "Loot #18",
+			"birthday": "10-24"
+		},
+		"19": {
+			"name": "Loot #19",
+			"birthday": "10-24"
+		},
+		"20": {
+			"name": "Loot #20",
+			"birthday": "10-24"
+		},
+		"21": {
+			"name": "Loot #21",
+			"birthday": "10-24"
+		},
+		"22": {
+			"name": "Loot #22",
+			"birthday": "10-24"
+		},
+		"23": {
+			"name": "Loot #23",
+			"birthday": "10-24"
+		},
+		"24": {
+			"name": "Loot #24",
+			"birthday": "10-24"
+		},
+		"25": {
+			"name": "Loot #25",
+			"birthday": "10-24"
+		},
+		"26": {
+			"name": "Loot #26",
+			"birthday": "10-24"
+		},
+		"27": {
+			"name": "Loot #27",
+			"birthday": "10-24"
+		},
+		"28": {
+			"name": "Loot #28",
+			"birthday": "10-24"
+		},
+		"29": {
+			"name": "Loot #29",
+			"birthday": "10-24"
+		},
+		"30": {
+			"name": "Loot #30",
+			"birthday": "10-24"
+		},
+		"31": {
+			"name": "Loot #31",
+			"birthday": "10-24"
+		},
+		"32": {
+			"name": "Loot #32",
+			"birthday": "10-24"
+		},
+		"33": {
+			"name": "Loot #33",
+			"birthday": "10-24"
+		},
+		"34": {
+			"name": "Loot #34",
+			"birthday": "10-24"
+		},
+		"35": {
+			"name": "Loot #35",
+			"birthday": "10-24"
+		},
+		"36": {
+			"name": "Loot #36",
+			"birthday": "10-24"
+		},
+		"37": {
+			"name": "Loot #37",
+			"birthday": "10-24"
+		},
+		"38": {
+			"name": "Loot #38",
+			"birthday": "10-24"
+		},
+		"39": {
+			"name": "Loot #39",
+			"birthday": "10-24"
+		},
+		"40": {
+			"name": "Loot #40",
+			"birthday": "10-24"
+		},
+		"41": {
+			"name": "Loot #41",
+			"birthday": "10-24"
+		},
+		"42": {
+			"name": "Loot #42",
+			"birthday": "10-24"
+		},
+		"43": {
+			"name": "Loot #43",
+			"birthday": "10-24"
+		},
+		"44": {
+			"name": "Loot #44",
+			"birthday": "10-24"
+		},
+		"45": {
+			"name": "Loot #45",
+			"birthday": "10-24"
+		},
+		"46": {
+			"name": "Loot #46",
+			"birthday": "10-24"
+		},
+		"47": {
+			"name": "Loot #47",
+			"birthday": "10-24"
+		},
+		"48": {
+			"name": "Loot #48",
+			"birthday": "10-24"
+		},
+		"49": {
+			"name": "Loot #49",
+			"birthday": "10-24"
+		},
+		"50": {
+			"name": "Loot #50",
+			"birthday": "10-24"
+		},
+		"51": {
+			"name": "Loot #51",
+			"birthday": "10-24"
+		},
+		"52": {
+			"name": "Loot #52",
+			"birthday": "10-24"
+		},
+		"53": {
+			"name": "Loot #53",
+			"birthday": "10-24"
+		},
+		"54": {
+			"name": "Loot #54",
+			"birthday": "10-24"
+		},
+		"55": {
+			"name": "Loot #55",
+			"birthday": "10-24"
+		},
+		"56": {
+			"name": "Loot #56",
+			"birthday": "10-24"
+		},
+		"57": {
+			"name": "Loot #57",
+			"birthday": "10-24"
+		},
+		"58": {
+			"name": "Loot #58",
+			"birthday": "10-24"
+		},
+		"59": {
+			"name": "Loot #59",
+			"birthday": "10-24"
+		},
+		"60": {
+			"name": "Loot #60",
+			"birthday": "10-24"
+		},
+		"61": {
+			"name": "Loot #61",
+			"birthday": "10-24"
+		},
+		"62": {
+			"name": "Loot #62",
+			"birthday": "10-24"
+		},
+		"63": {
+			"name": "Loot #63",
+			"birthday": "10-24"
+		},
+		"64": {
+			"name": "Loot #64",
+			"birthday": "10-24"
+		},
+		"65": {
+			"name": "Loot #65",
+			"birthday": "10-24"
+		},
+		"66": {
+			"name": "Loot #66",
+			"birthday": "10-24"
+		},
+		"67": {
+			"name": "Loot #67",
+			"birthday": "10-24"
+		},
+		"68": {
+			"name": "Loot #68",
+			"birthday": "10-24"
+		},
+		"69": {
+			"name": "Loot #69",
+			"birthday": "10-24"
+		},
+		"70": {
+			"name": "Loot #70",
+			"birthday": "10-24"
+		},
+		"71": {
+			"name": "Loot #71",
+			"birthday": "10-24"
+		},
+		"72": {
+			"name": "Loot #72",
+			"birthday": "10-24"
+		},
+		"73": {
+			"name": "Loot #73",
+			"birthday": "10-24"
+		},
+		"74": {
+			"name": "Loot #74",
+			"birthday": "10-24"
+		},
+		"75": {
+			"name": "Loot #75",
+			"birthday": "10-24"
+		},
+		"76": {
+			"name": "Loot #76",
+			"birthday": "10-24"
+		},
+		"77": {
+			"name": "Loot #77",
+			"birthday": "10-24"
+		},
+		"78": {
+			"name": "Loot #78",
+			"birthday": "10-24"
+		},
+		"79": {
+			"name": "Loot #79",
+			"birthday": "10-24"
+		},
+		"80": {
+			"name": "Loot #80",
+			"birthday": "10-24"
+		},
+		"81": {
+			"name": "Loot #81",
+			"birthday": "10-24"
+		},
+		"82": {
+			"name": "Loot #82",
+			"birthday": "10-24"
+		},
+		"83": {
+			"name": "Loot #83",
+			"birthday": "10-24"
+		},
+		"84": {
+			"name": "Loot #84",
+			"birthday": "10-24"
+		},
+		"85": {
+			"name": "Loot #85",
+			"birthday": "10-24"
+		},
+		"86": {
+			"name": "Loot #86",
+			"birthday": "10-24"
+		},
+		"87": {
+			"name": "Loot #87",
+			"birthday": "10-24"
+		},
+		"88": {
+			"name": "Loot #88",
+			"birthday": "10-24"
+		},
+		"89": {
+			"name": "Loot #89",
+			"birthday": "10-24"
+		},
+		"90": {
+			"name": "Loot #90",
+			"birthday": "10-24"
+		},
+		"91": {
+			"name": "Loot #91",
+			"birthday": "10-24"
+		},
+		"92": {
+			"name": "Loot #92",
+			"birthday": "10-24"
+		},
+		"93": {
+			"name": "Loot #93",
+			"birthday": "10-24"
+		},
+		"94": {
+			"name": "Loot #94",
+			"birthday": "10-24"
+		},
+		"95": {
+			"name": "Loot #95",
+			"birthday": "10-24"
+		},
+		"96": {
+			"name": "Loot #96",
+			"birthday": "10-24"
+		},
+		"97": {
+			"name": "Loot #97",
+			"birthday": "10-24"
+		},
+		"98": {
+			"name": "Loot #98",
+			"birthday": "10-24"
+		},
+		"99": {
+			"name": "Loot #99",
+			"birthday": "10-24"
 		}
 }
