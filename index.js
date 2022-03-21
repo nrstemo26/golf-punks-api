d970ce6225710eff5d1bfce8ad9eb385340b463b
DeSoDog <ty@deso.org>
2022-03-21 12:16:10 -0500

    Added trunks

--- index.js
+++ index.js
@@ -62,5 +62,5 @@ app.get("/api/trunks/:trunk", function (req, res) {
 });
 
 app.listen(app.get("port"), function () {
-  console.log("Node app is running on port", app.get("port"));
+  console.log("Node app is running on port ", app.get("port"));
 });
