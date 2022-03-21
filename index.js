979926a2560d84de55c214250ba3341b43a6094c
DeSoDog <ty@deso.org>
2022-03-21 18:25:46 -0500

    Api endpoint cleanup

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const path = require("path");
-const { HOST } = require("./src/constants");
+// const { HOST } = require("./src/constants");
 const cors = require("cors");
 
-const db = require("./src/db.js");
+// const db = require("./src/db.js");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const app = express()
   .set("port", PORT)
@@ -15,43 +15,46 @@ const app = express()
 app.use(cors());
 
 app.get("/", function (req, res) {
-  res.send("Get ready for OpenSea!");
+  res.send("Surf Punks 2022");
 });
 
-app.get("/api/all", async function (req, res) {
-  console.log("oy?");
-  res.send(JSON.stringify(db, null, 2));
-});
+// app.get("/api/all", async function (req, res) {
+//   console.log("oy?");
+//   res.send(JSON.stringify(db, null, 2));
+// });
 
-app.get("/api/token/:token_id", function (req, res) {
-  const bucketURL = `https://golf-punks.s3.amazonaws.com/images/`;
-  const tokenId = parseInt(req.params.token_id).toString();
-  const golfPunk = db[tokenId];
-  const host = req.get("host");
-  const golfPunkMetaData = {
-    name: golfPunk.name,
-    symbol: "GolfPunks",
-    background_color: "somehex", // probably need this
-    image: golfPunk.image,
-    description: "golf punks!",
-    attributes: [
-      { trait_type: "hat", value: golfPunk.attribute1 },
-      { trait_type: "hat", value: golfPunk.attribute2 },
-      { trait_type: "hat", value: golfPunk.attribute3 },
-      { trait_type: "hat", value: golfPunk.attribute4 },
-      { trait_type: "hat", value: golfPunk.attribute5 },
-    ],
-  };
-
-  res.send(JSON.stringify(golfPunkMetaData, null, 4));
-});
-
-app.get("/api/images/:surfer", function (req, res) {
+/* Gets metadata for a surfer
+ */
+app.get("/api/surfer/:surfer", function (req, res) {
   const surfer = req.params.surfer.toString();
-  res.sendFile(`./public/images/surfers/${surfer}.jpg`, { root: __dirname });
+  const metadata = {
+    name: `${surfer}`,
+    symbol: "SurfPunks",
+    background_color: "somehex", // probably need this
+    image: `${req.headers.host}/api/images/surfers/${surfer}`,
+    description: "surf punks (get description from conrad",
+    attributes: [],
+  };
+  res.send(JSON.stringify(metadata, null, 4));
+});
+/* Gets metadata for a surfer that has not been revealed */
+app.get("/api/trunks/:trunkType/:tokenId", function (req, res) {
+  const trunkType = req.params.trunkType.toString();
+  if (!["trunk-norm", "trunk-legacy"].includes(trunkType)) {
+    throw new Error("invalid request");
+  }
+  const tokenId = req.params.tokenId.toString();
+  const metadata = {
+    name: `${tokenId}`,
+    symbol: "SurfPunks",
+    image: `${req.headers.host}/api/images/trunks/${trunkType}`,
+    description: "surf punks (get description from conrad)",
+    attributes: [],
+  };
+  res.send(JSON.stringify(metadata, null, 4));
 });
 
-app.get("/api/trunks/:trunk", function (req, res) {
+app.get("/api/images/trunks/:trunk", function (req, res) {
   const trunk = req.params.trunk.toString();
   res.sendFile(`./public/images/trunks/${trunk}.gif`, {
     root: __dirname,
@@ -59,6 +62,11 @@ app.get("/api/trunks/:trunk", function (req, res) {
       "Cache-Control": "no-cache",
     },
   });
+});
+
+app.get("/api/images/surfers/:surfer", function (req, res) {
+  const surfer = req.params.surfer.toString();
+  res.sendFile(`./public/images/surfers/${surfer}.jpg`, { root: __dirname });
 });
 
 app.listen(app.get("port"), function () {
